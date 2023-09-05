ff736ed9aa10668eb004c671533a8f42685a67b2
Songonha <105191398+songonha@users.noreply.github.com>
2023-09-05 23:21:30 +0700

    Update BonsaiCertification.sol

--- origin/contracts/BonsaiCertification.sol
+++ origin/contracts/BonsaiCertification.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract BonsaiCertification {
+    string public bonsaiInfo;
+
+    function updateBonsaiInfo(string memory newInfo) public {
+        bonsaiInfo = newInfo;
+    }
+}
